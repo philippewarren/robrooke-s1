42c47712705f99d2dc2e8db605c4fc5bc66008e8
philippewarren <philippewarren31@gmail.com>
2019-10-13 14:53:31 -0400

    Ajout de l'arrêt d'urgence

--- src/init_robot.cpp
+++ src/init_robot.cpp
@@ -3,10 +3,27 @@
 char Bob  = 'I';
 extern float largeurEss;
 
+const byte brocheInterrupt = 21;
+const byte brocheRestart = 20;
+
 void initialiserBob()
 {
   Bob = EEPROM.read(0);
+  pinMode(brocheInterrupt, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(brocheInterrupt), arretUrgence, FALLING);
   largeurEss = Bob=='A' ? 18.2 : 18.1;
 // DIST_90 = (largeurParc-largeurEss)/2;    //la distance que Bob doit avancer pour un virage de 90 degres
 // DIST_45 = DIST_90*TAN_22_5;              //la distance que Bob doit avancer pour un virage de 45 degres
+}
+
+//ISR d'arrêt d'urgence
+void arretUrgence()
+{
+    arreterDeuxMoteurs();
+    AX_BuzzerON();
+    while (digitalRead(brocheRestart)!=HIGH)
+    {
+        //Attend qu'on reboot avec le bouton
+    }
+    AX_BuzzerOFF();
 }
--- src/init_robot.h
+++ src/init_robot.h
@@ -1,13 +1,19 @@
 #ifndef INIT_ROBOT_H
 #define INIT_ROBOT_H
+
+#include <Arduino.h>
+#include <LibRobus.h>
 #include <EEPROM.h>
+
 #include "conversions.h"
 #include "mouvement.h"
-#include <LibRobus.h>
 #include "interactions.h"
+
 
 extern char Bob;
 
 void initialiserBob();
+
+void arretUrgence();
 
 #endif
