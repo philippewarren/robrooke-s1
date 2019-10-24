906214c6166f93c839bb7af3cc2767c8cd0e6795
Philippe Warren <philippewarren31@gmail.com>
2019-10-23 22:03:45 -0400

    Changement des ports des boutons pour ne pas nuire à d'autres fonctionnalités du Arduino-X ou de Robus

--- src/init_robot.cpp
+++ src/init_robot.cpp
@@ -8,8 +8,8 @@ void initialiserBob()
 {
     BoardInit();
     initialiserPortsDEL();
-    initialiserPortsBoutons();
-    initialiserPortsInterrupt();
+    //initialiserPortsBoutons();
+    //initialiserPortsInterrupt();
     Bob = EEPROM.read(0);
     largeurEss = Bob=='A' ? 18.2 : 18.1;
     // DIST_90 = (largeurParc-largeurEss)/2;    //la distance que Bob doit avancer pour un virage de 90 degres
