43947c73848d46762d7f1e5fd09c0465c4ee4b47
vililichi <keven0230@live.ca>
2019-10-24 08:50:01 -0400

    correction d'un truc étrange

--- src/loop_normal.h
+++ src/loop_normal.h
@@ -5,11 +5,8 @@
 #include "interactions.h"
 #include "capteur_ir.h"
 #include "suiveur_ligne.h"
-<<<<<<< HEAD
 #include "mouvement.h"
-=======
 
->>>>>>> 906214c6166f93c839bb7af3cc2767c8cd0e6795
 //Loop standard
 void loopNormal();
 
