91f71b6019c0182332e4ca3891952519b7b25497
vililichi <keven0230@live.ca>
2019-11-01 13:50:03 -0400

    couleur

--- src/loop_A.cpp
+++ src/loop_A.cpp
@@ -1,10 +1,13 @@
 #include "loop_A.h"
+#include "init_robot.h"
 #include "Arduino.h"
 #include "interactions.h"
 #include "mouvement.h"
 #include "suiveur_ligne.h"
 
-
+//COULEUR_DEPART
+//COULEUR_FIN
+//COULEUR_OCTOGONE
 
 
 void loopOctogoneA()
