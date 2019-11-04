992b8ef5ac08bb594d5daad1f7ed1906e2b6e7e9
philippewarren <philippewarren31@gmail.com>
2019-11-04 14:41:17 -0500

    Création d'une fonction bloquante pour BobB, avec un délais de test actuellement, et loop_normal changé pour ajuster les servos

--- src/loop_normal.cpp
+++ src/loop_normal.cpp
@@ -1,10 +1,29 @@
 #include "loop_normal.h"
 
+int angle = 0;
 
 void loopNormal()
 {
-    if(loopEstCliqueEtRelache(3)) octogoneA(VERT);
-    if(loopEstCliqueEtRelache(0)) octogoneA(BLEU);
-    if(loopEstCliqueEtRelache(1)) octogoneA(JAUNE);
+    changerAngleServo(PINCE, angle);
+    Serial.println(angle);
+    // if (Bob=='B')
+    // {
+    //     if(loopEstCliqueEtRelache(3)) octogoneB(VERT);
+    //     if(loopEstCliqueEtRelache(0)) octogoneB(BLEU);
+    //     if(loopEstCliqueEtRelache(1)) octogoneB(JAUNE);
+    // }
+    // else
+    // {
+    //     if(loopEstCliqueEtRelache(3)) octogoneA(VERT);
+    //     if(loopEstCliqueEtRelache(0)) octogoneA(BLEU);
+    //     if(loopEstCliqueEtRelache(1)) octogoneA(JAUNE);
+    // }
+
+    if (loopEstCliqueEtRelache(0)) angle += 5;
+    if (loopEstCliqueEtRelache(1)) angle -= 5;
+    if (loopEstCliqueEtRelache(3)) angle = 0;
+    angle = (angle>180) ? 0 : angle;
+    angle = (angle<0) ? 180 : angle;
+
 
 }
