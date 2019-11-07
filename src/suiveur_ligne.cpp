bdee5febbfdceb5c3cde499df3c30b2e9d5364c3
philippewarren <philippewarren31@gmail.com>
2019-11-07 10:58:55 -0500

    a

--- src/suiveur_ligne.cpp
+++ src/suiveur_ligne.cpp
@@ -330,7 +330,7 @@ void estLigneHuit(int lectures[8])
 {
   for (int i=0; i<8; i++)
   {
-    lectures[i] = (lectures[i] > 325) ? 1 : /*(lectures[i] > 250) ? 2 :*/ 0;//(350 rouge ok; vert ok; problème vert?)
+    lectures[i] = (lectures[i] > 400) ? 1 : /*(lectures[i] > 250) ? 2 :*/ 0;//(350 rouge ok; vert ok; problème vert?)
   }
 }
 
